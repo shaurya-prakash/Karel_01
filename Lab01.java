e141cbf9c2a545ff6d2ed76f82bc521c3017fa8c
shaurya-prakash <shaurya_prakash@asl.org>
2014-10-29 17:22:28 +0000

    Lab01

--- Lab01.java
+++ Lab01.java
@@ -1,9 +1,9 @@
 /**
 *
-* Description of the program goes here  // provide a brief description
+* Putting School Books in the Storage Room (Lab01 in Unit1, fcps.karel)
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author Shaurya Prakash
+* @version 24/10/2014
 */
 
 import edu.fcps.karel2.Display;
@@ -12,6 +12,74 @@ import edu.fcps.karel2.Robot;
 public class Lab01 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
-     }
+            
+            Display.openWorld("maps/school.map");
+            Display.setSize(10, 10);
+            
+            Robot lisa = new Robot();
+            Robot pete = new Robot(4, 5, Display.SOUTH, 0);
+            
+            lisa.move();
+            lisa.move(); 
+            lisa.turnLeft();
+            lisa.move();
+            lisa.turnLeft();
+            lisa.move(); 
+            lisa.pickBeeper();
+            lisa.turnLeft();
+            lisa.turnLeft();
+            lisa.move();
+            lisa.turnLeft();
+            lisa.turnLeft();
+            lisa.turnLeft();
+            lisa.move(); 
+            lisa.turnLeft();
+            lisa.move(); 
+            lisa.move(); 
+            lisa.move(); 
+            lisa.turnLeft();
+            lisa.move(); 
+            lisa.move(); 
+            lisa.move();
+            lisa.turnLeft();
+            lisa.move();
+            lisa.move();
+            lisa.turnLeft();
+            lisa.turnLeft();
+            lisa.turnLeft();
+            lisa.move();
+            lisa.putBeeper();
+            pete.pickBeeper();
+            pete.move();
+            pete.turnLeft();   
+            pete.move();
+            pete.move();
+            pete.turnLeft();
+            pete.move();
+            pete.move();
+            pete.turnLeft();
+            pete.turnLeft();
+            pete.turnLeft();
+            pete.move();
+            pete.move();
+            pete.turnLeft();
+            pete.move();
+            pete.turnLeft();
+            pete.move();
+            pete.move();
+            pete.turnLeft();
+            pete.turnLeft();
+            pete.turnLeft();
+            pete.move();
+            pete.move();
+            pete.turnLeft();
+            pete.move();
+            pete.move();
+            pete.putBeeper();
+            pete.turnLeft();
+            pete.turnLeft();
+            pete.move();
+      
+   }
+   
 }
